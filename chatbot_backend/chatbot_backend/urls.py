c7c6009a5af1ce561bea18a5a4d84cbe3d856830
ajme-abes <ajmelabes@gmail.com>
2026-01-09 22:34:14 +0300

    fix: remove problematic views.py that triggers spaCy import

--- chatbot_backend/chatbot_backend/urls.py
+++ chatbot_backend/chatbot_backend/urls.py
@@ -2,11 +2,8 @@ import os
 from django.urls import path
 from django.views.generic import TemplateView
 
-# Use deployment views if in deployment environment
-if os.environ.get('RENDER') or os.environ.get('USE_SIMPLE_PROCESSOR'):
-    from chatapi.views_deployment import ChatView, weather_api, PerformanceView
-else:
-    from chatapi.views import ChatView, weather_api, PerformanceView
+# ALWAYS use deployment views for Render - no fallbacks!
+from chatapi.views_deployment import ChatView, weather_api, PerformanceView
 
 urlpatterns = [
     path('api/chat/', ChatView.as_view(), name='chat'),
